58b86ae1663cb02e380b1e5ff13a8c6d29fb6c8e
LucaBrivio183 <l.brivio4@gmail.com>
2023-03-16 16:12:22 +0100

    feat: first scaffolding

---
+++ MILESTONE #1/main.js
@@ -0,0 +1,12 @@
+const  { createApp } = Vue; //initialize Vue
+
+createApp ({
+    data() {  //Vue section for data
+        return {
+
+        }     
+    }, 
+    methods: { //Vue section for function
+        
+    },   
+}).mount('#app');
