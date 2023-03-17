c576acaff36b00b0d4d1ebe0ceb737c3799953ee
LucaBrivio183 <l.brivio4@gmail.com>
2023-03-17 10:12:55 +0100

    fix: folder name

--- MILESTONE #1/main.js
+++ MILESTONE #1/main.js
@@ -3,7 +3,10 @@ const  { createApp } = Vue; //initialize Vue
 createApp ({
     data() {  //Vue section for data
         return {
-
+            user: {
+                name:'Sofia',
+                
+            }
         }     
     }, 
     methods: { //Vue section for function
